a39642962d17cea8043139c975929f079834ca86
tottiwxt <331702110@qq.com>
2018-04-07 22:03:35 +0200

    FindNumbersWithSum
    FindNumbersWithSum

---
+++ src/FindNumbersWithSum.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+
+public class FindNumbersWithSum {
+
+	public ArrayList<Integer> FindNumbersWithSum(int [] array,int sum) {
+        ArrayList<Integer> res =new ArrayList<Integer>();
+		int i = 0; 
+		int j = array.length-1;
+		int min = 999999999;
+		while(i!=j){
+			if(array[i]+array[j]==sum){
+				if(array[i]*array[j]<min){
+					min = array[i]*array[j];
+					if(!res.isEmpty()){
+					res.set(0,array[i]);
+					res.set(1,array[j]);
+					}
+					else{
+						res.add(array[i]);
+						res.add(array[j]);
+					}
+				}
+				i++;
+			}
+			else if(array[i]+array[j]<sum){
+				i++;
+			}
+			else if(array[i]+array[j]>sum){
+				j--;
+			}
+		}
+		
+		
+		return res;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
