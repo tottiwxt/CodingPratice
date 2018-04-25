96c435825e5206ba8388072dd00964388aa52baa
tottiwxt <331702110@qq.com>
2018-04-25 14:51:08 +0200

    浅拷贝
    浅拷贝：对引用数据类型只是进行了引用的传递，而没有真实的创建一个新的对象

--- src/draft/DeepShallowCopy.java
+++ src/draft/DeepShallowCopy.java
@@ -8,19 +8,43 @@ public class DeepShallowCopy {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Person p1 = new Person("xutao", 24);
-		Person p2 = p1;
+		Person p1 = new Person("xutao", 24,new Pet("dog"));
+//		Person p2 = p1;
+		Person p2 = (Person) p1.clone();
 		p2.name = "jiaping";
-		System.out.println(p1.name);
+		p2.pet.name = "cat";
+		System.out.println("p1的名为:"+p1.name);
+		System.out.println("p1的宠物名为:"+p1.pet.name);
+		System.out.println("p2的宠物名为:"+p2.pet.name); //宠物名一样，浅拷贝
 	}
 
 }
- class Person{
+ class Person implements Cloneable{
 	String name;
 	int age;
-	public Person(String name,int age) {
+	Pet pet;
+	public Object clone(){
+		Person b =null;
+		try{
+			b = (Person)super.clone();
+		}catch (CloneNotSupportedException  e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return b;
+	}
+	public Person(String name,int age,Pet pet) {
 		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.age = age;
+		this.pet = pet;
 	}
+	
 }
+
+ class Pet{
+	 String name;
+	 Pet(String name){
+		 this.name = name ;
+	 }
+ }
